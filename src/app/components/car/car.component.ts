2eacd3e7169a4a150325035d2408c1da8392cc37
Ali Alpay <alialpay96@hotmail.com>
2021-03-17 20:34:53 +0300

    17. gün commit 2

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Car } from 'src/app/models/car';
 
 @Component({
   selector: 'app-car',
@@ -7,13 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarComponent implements OnInit {
 
-  car1 = { id: 1, brandName: 'Togg', colorName: 'Mavi', modelYear: 2003, description: 'Açıklama Yapılacak1' }
-  car2 = { id: 2, brandName: 'Mercedes', colorName: 'Sarı', modelYear: 2003, description: 'Açıklama Yapılacak2' }
-  car3 = { id: 3, brandName: 'Bmw', colorName: 'Yeşil', modelYear: 2003, description: 'Açıklama Yapılacak3' }
-  car4 = { id: 4, brandName: 'Toyota', colorName: 'Kırmızı', modelYear: 2003, description: 'Açıklama Yapılacak4' }
-  car5 = { id: 5, brandName: 'Fiat', colorName: 'Beyaz', modelYear: 2003, description: 'Açıklama Yapılacak5' }
-
-  cars = [this.car1, this.car2, this.car3, this.car4, this.car5]
+  cars: Car[] = []
 
   constructor() { }
 
