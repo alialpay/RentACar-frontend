f969a7f90c9fff8c797178812d6a8bb4922538c0
Ali Alpay <alialpay96@hotmail.com>
2021-03-29 00:00:34 +0300

    jquery ve tüm ürünler

--- src/app/components/brand/brand.component.ts
+++ src/app/components/brand/brand.component.ts
@@ -36,4 +36,11 @@ export class BrandComponent implements OnInit {
       return "list-group-item"
     }
   }
+  getAllBrandClass(){
+    if(!this.currentBrand){
+      return "list-group-item active"
+    }else{
+      return "list-group-item"
+    }
+  }
 }
